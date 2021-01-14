dd41d133affd3e50ee78828105a8fb1e8a4c1d05
Joonsky18 <45098147+Joonsky18@users.noreply.github.com>
2021-01-14 15:11:59 +0700

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-const unityInstance = UnityLoader.instantiate("unityContainer", "Build/ARWT_Squirrel.json");
+const unityInstance = UnityLoader.instantiate("unityContainer", "Build/ARWT-TrexSkull.json");
 let isCameraReady = false;
 let isDetectionManagerReady = false;
 let gl = null;
