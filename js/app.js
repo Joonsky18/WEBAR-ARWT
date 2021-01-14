8fe635c548ed521723b4f035645e1a3c6096fd52
Joonsky18 <45098147+Joonsky18@users.noreply.github.com>
2021-01-14 14:02:33 +0700

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-const unityInstance = UnityLoader.instantiate("unityContainer", "Build/ARWT Fiat500.json");
+const unityInstance = UnityLoader.instantiate("unityContainer", "Build/ARWT_Squirrel.json");
 let isCameraReady = false;
 let isDetectionManagerReady = false;
 let gl = null;
